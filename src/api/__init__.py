6023abc19bcdc3f04e765a6bf7b91af9ef2c8062
= <adiiiiikhan1998@outlook.com>
2021-11-18 01:14:12 +0500

    Project Setup

---
+++ src/api/__init__.py
@@ -0,0 +1,13 @@
+import logging
+import os
+from http import HTTPStatus
+
+from .user.endpoint import api as user_api
+from flask import Blueprint
+from flask_restx import Api
+from werkzeug.exceptions import BadRequest, Forbidden, NotFound, Unauthorized
+
+blueprint = Blueprint("api", __name__)
+api = Api(blueprint, title="Renso Api's", version="0.1", description="Renso official api's")
+
+api.add_namespace(user_api)
