49f34174cb72bb9316d6344441f7bb767019ef6e
= <adeel.khan@tkxel.io>
2022-03-22 19:53:20 +0500

    voucher ADDED

--- src/api/booking/endpoint.py
+++ src/api/booking/endpoint.py
@@ -103,6 +103,7 @@ class booking_list(Resource):
         args = request.args
         booking_ids = args.get("booking_is").split(",")
         price_factor = 100
+        voucher = None
         if "voucher" in args.keys():
             voucher = Voucher.get_voucher_by_code(args.get("voucher"))
             if voucher:
