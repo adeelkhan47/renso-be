75ad65faa2a90ac92c1ca907f1beca50bfc98e50
= <adeel.khan@tkxel.io>
2022-03-31 22:49:43 +0500

    reverted

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,18 +1,18 @@
-#!/bin/sh
-
-set -e
-cd /app/
-. .venv/bin/activate
-
-cd src
-
-while ! flask db upgrade
-do
-     echo "Retry..."
-     sleep 1
-done
-
-#exec celery -A tasks.email worker -l INFO --pool=solo &
-#exec celery -A tasks.email beat -l INFO &
-#exec celery -A tasks.queue beat -l INFO &
-exec gunicorn --bind 0.0.0.0:5000 --forwarded-allow-ips='*' wsgi:app
+##!/bin/sh
+#
+#set -e
+#cd /app/
+#. .venv/bin/activate
+#
+#cd src
+#
+#while ! flask db upgrade
+#do
+#     echo "Retry..."
+#     sleep 1
+#done
+#
+##exec celery -A tasks.email worker -l INFO --pool=solo &
+##exec celery -A tasks.email beat -l INFO &
+##exec celery -A tasks.queue beat -l INFO &
+#exec gunicorn --bind 0.0.0.0:5000 --forwarded-allow-ips='*' wsgi:app
