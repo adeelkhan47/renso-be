e1ad8f28a3e3b53a9575dd771519d2b0a2aca780
= <adeel.khan@tkxel.io>
2022-04-05 22:31:34 +0500

    patch added

--- src/app.py
+++ src/app.py
@@ -21,7 +21,7 @@ db.init_app(app)
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,HEAD,OPTIONS')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE,HEAD,OPTIONS')
     return response
 
 
