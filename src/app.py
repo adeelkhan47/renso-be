ba170471fcc56d537c09f52bac42be9c0ea10cc5
= <adiiiiikhan1998@outlook.com>
2022-04-26 00:49:55 +0500

    cors try

--- src/app.py
+++ src/app.py
@@ -19,9 +19,8 @@ db.init_app(app)
 
 @app.after_request
 def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', ["http://rensofe.s3-website-us-east-1.amazonaws.com",
-                                                         "http://rensositefe.s3-website-us-east-1.amazonaws.com",
-                                                         "http://renso.de",
+    response.headers.add('Access-Control-Allow-Origin', ["https://schlei.renso.de", "https://rad.renso.de",
+                                                         "https://renso.de",
                                                          "http://localhost:8080/", "http://localhost:8081/"])
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE,HEAD,OPTIONS')
