daecfba28444f996cf13b42dabd963d0995252e9
= <adiiiiikhan1998@outlook.com>
2022-05-01 02:55:30 +0500

    quick fix

--- src/model/item_subtype.py
+++ src/model/item_subtype.py
@@ -17,7 +17,7 @@ class ItemSubType(Base, db.Model):
     same_price_days = Column(Integer, nullable=True, unique=False, default=1)
 
 
-    items = relationship("Item", backref="item_subotype")
+    items = relationship("Item", backref="item_subtype")
     associate_email_subtypes = relationship("AssociateEmailSubtype", backref="item_subtype")
     user_id = Column(Integer, ForeignKey("user.id", ondelete="CASCADE"), nullable=False, index=True)
     itemTypeExtras = relationship("ItemTypeExtra", backref="item_subtype")
