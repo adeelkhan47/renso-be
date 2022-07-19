323e359289aa83d5ab1400c9e43ee004d07f3812
= <adiiiiikhan1998@outlook.com>
2022-07-19 13:33:13 +0200

    debug v15

--- src/api/company/endpoint.py
+++ src/api/company/endpoint.py
@@ -52,7 +52,7 @@ class item_by_id(Resource):
     def patch(self, company_id):
         data = api.payload.copy()
         if "is_deleted" not in data.keys():
-            data["is_deleted"] = "False"
+            data["is_deleted"] = bool("false")
         Company.update(company_id, data)
         item = Company.query_by_id(company_id)
         return response_structure(item), 200
